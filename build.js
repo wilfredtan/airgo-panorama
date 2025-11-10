10ebb58556b7f0ff17a7fee7ca5024e3420a487f
Wilfred Tan <23373389+wilfredtan@users.noreply.github.com>
2025-11-10 13:58:41 +0800

    Fix env issue

--- build.js
+++ build.js
@@ -1,4 +1,12 @@
 import * as esbuild from 'esbuild';
 import config from './esbuild.config.js';
 
+// Get API URL from command line argument or environment variable
+const apiUrl = process.argv[2] || process.env.REACT_APP_API_BASE_URL; // eslint-disable-line no-undef
+
+// Override the API URL in the config if provided
+if (apiUrl) {
+  config.define['process.env.REACT_APP_API_BASE_URL'] = JSON.stringify(apiUrl);
+}
+
 esbuild.build(config).catch(() => process.exit(1)); // eslint-disable-line no-undef
